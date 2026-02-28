b164b3dbd6e73c965174844010ef47c9669d746c
Savi <seany@eclipso.de>
2026-02-28 19:45:44 +0100

    Create colors.d

---
+++ V1/AST_generator/cli/colors.d
@@ -0,0 +1,11 @@
+module cli.colors;
+
+public immutable string cyan    = "\033[36m";
+public immutable string skyBlue = "\033[38;5;117m";
+public immutable string green   = "\033[32m";
+public immutable string orange  = "\033[38;5;208m";
+public immutable string brightRed = "\033[91m";
+public immutable string brightYellow = "\033[93m";
+
+public immutable string reset   = "\033[0m";
+public immutable string defaultColor = "\033[39m";
